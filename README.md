77a540d6aafc39548817370016248342409ffbed
Ray Myers <rmyers@indeed.com>
2023-05-09 17:23:44 -0500

    Formatting and folder structure

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# menderbot
+# Menderbot
 AI-powered command line tool for working with legacy code
+
+## Developing
+
+```
+pipenv shell
+```
+### Running
+```
+python -m menderbot
+```
+### Pre-commit
+
+```
+black .
+```
---
+++ menderbot/llm.py
@@ -0,0 +1,49 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+INSTRUCTIONS = (
+    """You are helpful electronic assistant with knowledge of Software Engineering."""
+)
+
+TEMPERATURE = 0.5
+MAX_TOKENS = 500
+FREQUENCY_PENALTY = 0
+PRESENCE_PENALTY = 0.6
+# limits how many questions we include in the prompt
+MAX_CONTEXT_QUESTIONS = 10
+
+
+def get_response(instructions, previous_questions_and_answers, new_question):
+    """Get a response from ChatCompletion
+
+    Args:
+        instructions: The instructions for the chat bot - this determines how it will behave
+        previous_questions_and_answers: Chat history
+        new_question: The new question to ask the bot
+
+    Returns:
+        The response text
+    """
+    # build the messages
+    messages = [
+        {"role": "system", "content": instructions},
+    ]
+    # add the previous questions and answers
+    for question, answer in previous_questions_and_answers[-MAX_CONTEXT_QUESTIONS:]:
+        messages.append({"role": "user", "content": question})
+        messages.append({"role": "assistant", "content": answer})
+    # add the new question
+    messages.append({"role": "user", "content": new_question})
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS,
+        top_p=1,
+        frequency_penalty=FREQUENCY_PENALTY,
+        presence_penalty=PRESENCE_PENALTY,
+    )
+    return completion.choices[0].message.content
